7913b3b7452f59583b7f364c6942b3a42aa04381
gasmsy2021 <96715179+gasmsy2021@users.noreply.github.com>
2023-07-19 08:37:16 +0100

    update server url

--- client/script.js
+++ client/script.js
@@ -83,7 +83,7 @@ const handleSubmit =async (e)=>{
     loader(messageDiv);
 
     //fetch data from the server -> bot's response
-    const response=await fetch('http://localhost:5000',{
+    const response=await fetch('https://code1-siml.onrender.com',{
         method:'POST',
         headers:{
             'Content-Type':'application/json'
